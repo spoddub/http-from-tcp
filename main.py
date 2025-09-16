bbc1b339073eaa20dd60db0669186c59b30043db
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-16 22:19:56 +0300

    read messages by 8 bites

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def main():
-    print("I hope I get the job!")
+import sys
+
+CHUNK_SIZE = 8
+
+
+def main() -> None:
+    with open("messages.txt", "rb") as f:
+        while True:
+            chunk = f.read(CHUNK_SIZE)
+            if not chunk:
+                break
+            sys.stdout.buffer.write(b"read: " + chunk + b"\n")
+            sys.stdout.buffer.flush()
 
 
 if __name__ == "__main__":
